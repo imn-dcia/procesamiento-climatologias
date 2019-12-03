24bdc8cb465675cfee41b83deb0fb5f1b9766b1c
Manuel Vargas <mfvargas@gmail.com>
2019-12-03 00:44:13 -0600

    Create recorte-climatologias.py

---
+++ recorte-climatologias.py
@@ -0,0 +1,29 @@
+import glob
+import subprocess
+import os
+
+ImageList = sorted(glob.glob('*.tiff'))
+
+Shapefile = 'Borde_CR_Oficial_IGN_2018_wgs84.shp'
+
+TargetRes = '0.00833333 0.00833333' # Defines the target resolution
+
+# Create output directory
+OutDir = './raster_recortados/'
+if not os.path.exists(OutDir):
+    os.makedirs(OutDir)
+
+for Image in ImageList:
+    print('Procesando ' + Image)
+
+    OutImage = OutDir + Image.replace('.tiff', '_recortado.tiff') # Defines Output Image
+
+    # Clip image
+    subprocess.call('gdalwarp -of GTiff -co "COMPRESS=DEFLATE" -tr '+TargetRes+ ' -cutline ' +Shapefile+ ' -crop_to_cutline ' +Image+ ' ' +OutImage, shell=True)
+
+    # Build image overviews
+    subprocess.call('gdaladdo --config COMPRESS_OVERVIEW DEFLATE ' +OutImage+ ' 2 4 8 16 32 64', shell=True)
+
+    print('Imagen procesada.' + '\n')
+
+print('Todas las imagenes fueron procesadas.')
